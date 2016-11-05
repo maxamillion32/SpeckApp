624053ea36c3b3b6930e8e4d61158ea30f40a0e2
Vitali Schneider <tastaturkaputt@googlemail.com>
2016-11-05 19:38:25 +0100

    VS: add remove beacons from machine functionality

--- src/pages/machineDetails/machineDetails.ts
+++ src/pages/machineDetails/machineDetails.ts
@@ -7,164 +7,182 @@ import { Pagebase } from '../base/pageBase';
 import { NativeStorage } from 'ionic-native';
 
 @Component({
-  selector: 'page-machineDetails',
-  templateUrl: 'machineDetails.html'
+    selector: 'page-machineDetails',
+    templateUrl: 'machineDetails.html'
 })
 export class MachineDetailsPage extends Pagebase {
 
-  machines: any;
-  selectedMachine: Machine;
-  isNewMachine: boolean;
+    machines: any;
+    selectedMachine: Machine;
+    isNewMachine: boolean;
 
-  alertCtrl: AlertController;
+    alertCtrl: AlertController;
 
-  beacon1: Beacon;
-  beacon2: Beacon;
+    beacon1: Beacon;
+    beacon2: Beacon;
 
-  constructor(public navCtrl: NavController, loadingCtrl: LoadingController, alertCtrl: AlertController, public navParams: NavParams) {
-    super(loadingCtrl, alertCtrl);
+    constructor(public navCtrl: NavController, loadingCtrl: LoadingController, alertCtrl: AlertController, public navParams: NavParams) {
+        super(loadingCtrl, alertCtrl);
 
-    this.alertCtrl = alertCtrl;
+        this.alertCtrl = alertCtrl;
 
-    this.machines = [];
-    this.selectedMachine = navParams.get('machine') as Machine;
-    this.isNewMachine = navParams.get('isNew') as boolean;
-    this.beacon1 = this.selectedMachine.assignedBeacons[0] as Beacon;
-    this.beacon2 = this.selectedMachine.assignedBeacons[1] as Beacon;
+        this.machines = [];
+        this.selectedMachine = navParams.get('machine') as Machine;
+        this.isNewMachine = navParams.get('isNew') as boolean;
+        this.beacon1 = this.selectedMachine.assignedBeacons[0] as Beacon;
+        this.beacon2 = this.selectedMachine.assignedBeacons[1] as Beacon;
 
-    NativeStorage.getItem('savedMachines')
-      .then(data => {
+        NativeStorage.getItem('savedMachines')
+            .then(data => {
 
-        this.machines = data;
+                this.machines = data;
 
-        if (this.isNewMachine) {
+                if (this.isNewMachine) {
 
-          this.machines.push(this.selectedMachine);
+                    this.machines.push(this.selectedMachine);
 
-          NativeStorage.setItem('savedMachines', this.machines)
-            .then(() => {
-              console.log('Machines stored!');
+                    NativeStorage.setItem('savedMachines', this.machines)
+                        .then(() => {
+                            console.log('Machines stored!');
+                        },
+                        error => console.error('Error storing item: ', error));
+                }
             },
-            error => console.error('Error storing item: ', error));
-        }
-      },
-      error => console.error('Error getting item: ', error));
+            error => console.error('Error getting item: ', error));
 
-  }
+    }
 
-  showEditDialog(name: string, type: string, description: string) {
+    showEditDialog(name: string, type: string, description: string) {
 
-    let prompt = this.alertCtrl.create({
-      title: 'Maschine bearbeiten',
-      message: "Bitte geben Sie die neuen Informationen ein:",
-      inputs: [
-        {
-          name: 'name',
-          placeholder: 'Name',
-          value: name
-        },
-        {
-          name: 'type',
-          placeholder: 'Typ',
-          value: type
-        },
-        {
-          name: 'description',
-          placeholder: 'Beschreibung',
-          value: description
-        },
-      ],
-      buttons: [
-        {
-          text: 'Abbrechen'
-        },
-        {
-          text: 'Speichern',
-          handler: data => {
+        let prompt = this.alertCtrl.create({
+            title: 'Maschine bearbeiten',
+            message: "Bitte geben Sie die neuen Informationen ein:",
+            inputs: [
+                {
+                    name: 'name',
+                    placeholder: 'Name',
+                    value: name
+                },
+                {
+                    name: 'type',
+                    placeholder: 'Typ',
+                    value: type
+                },
+                {
+                    name: 'description',
+                    placeholder: 'Beschreibung',
+                    value: description
+                },
+            ],
+            buttons: [
+                {
+                    text: 'Abbrechen'
+                },
+                {
+                    text: 'Speichern',
+                    handler: data => {
 
-            for (var _i = 0; _i < this.machines.length; _i++) {
+                        for (var _i = 0; _i < this.machines.length; _i++) {
 
-              if (this.machines[_i].id === this.selectedMachine.id) {
+                            if (this.machines[_i].id === this.selectedMachine.id) {
 
-                this.selectedMachine.name = data.name;
-                this.selectedMachine.type = data.type;
-                this.selectedMachine.description = data.description;
+                                this.selectedMachine.name = data.name;
+                                this.selectedMachine.type = data.type;
+                                this.selectedMachine.description = data.description;
 
-                this.machines[_i] = this.selectedMachine;
+                                this.machines[_i] = this.selectedMachine;
 
+                                break;
+
+                            }
+                        }
+
+                        NativeStorage.setItem('savedMachines', this.machines)
+                            .then(() => {
+                                console.log('Machines stored!');
+                            },
+                            error => console.error('Error storing item: ', error));
+
+                    }
+                }
+            ]
+        });
+        prompt.present();
+    }
+
+    addFirstBeaconToMachine() {
+
+    }
+
+    addSecondBeaconToMachine() {
+
+    }
+
+    removeFirstBeaconFromMachine() {
+
+        this.beacon1.isDummy = true;
+
+        for (var _i = 0; _i < this.machines.length; _i++) {
+
+            if (this.machines[_i].id === this.selectedMachine.id) {
+
+                this.machines[_i].assignedBeacons[0].isDummy = true;
                 break;
 
-              }
             }
-
-            NativeStorage.setItem('savedMachines', this.machines)
-              .then(() => {
-                console.log('Machines stored!');
-              },
-              error => console.error('Error storing item: ', error));
-
-          }
         }
-      ]
-    });
-    prompt.present();
-  }
 
-  ionViewWillEnter() {
+        NativeStorage.setItem('savedMachines', this.machines)
+            .then(() => {
+                console.log('Machines stored!');
+            },
+            error => console.error('Error storing item: ', error));
 
-    console.log('Im here...');
-
-  }
-
-  assignBeacon() {
-
-  }
-
-  unassignBeacon(beaconToUnassign: Beacon) {
-
-    let beaconIndex = this.selectedMachine.assignedBeacons.indexOf(beaconToUnassign, 0);
-    if (beaconIndex > -1) {
-      this.selectedMachine.assignedBeacons[beaconIndex].isDummy = true;
-    }
-
-    // Save changes to the native storage
-    if (this.machines == null) {
-
-      this.machines = [];
-      this.machines.push(this.selectedMachine);
-
-    } else {
-
-      let machineIndex = this.machines.indexOf(this.selectedMachine, 0);
-      if (machineIndex > -1) {
-        this.machines[machineIndex] = this.selectedMachine;
-      }
 
     }
 
-    this.saveItem('savedMachines', this.machines);
-  }
+    removeSecondBeaconFromMachine() {
 
-  deleteMachine() {
+        this.beacon2.isDummy = true;
 
-    for (var _i = 0; _i < this.machines.length; _i++) {
+        for (var _i = 0; _i < this.machines.length; _i++) {
 
-      if (this.machines[_i].id === this.selectedMachine.id) {
+            if (this.machines[_i].id === this.selectedMachine.id) {
 
-        this.machines.splice(_i, 1);
-        break;
+                this.machines[_i].assignedBeacons[1].isDummy = true;
+                break;
 
-      }
+            }
+        }
+
+        NativeStorage.setItem('savedMachines', this.machines)
+            .then(() => {
+                console.log('Machines stored!');
+            },
+            error => console.error('Error storing item: ', error));
+
     }
 
-    NativeStorage.setItem('savedMachines', this.machines)
-      .then(() => {
-        console.log('Machines stored!');
-      },
-      error => console.error('Error storing item: ', error));
+    deleteMachine() {
 
-    // Navigate back
-    this.navCtrl.pop();
+        for (var _i = 0; _i < this.machines.length; _i++) {
 
-  }
+            if (this.machines[_i].id === this.selectedMachine.id) {
+
+                this.machines.splice(_i, 1);
+                break;
+
+            }
+        }
+
+        NativeStorage.setItem('savedMachines', this.machines)
+            .then(() => {
+                console.log('Machines stored!');
+            },
+            error => console.error('Error storing item: ', error));
+
+        // Navigate back
+        this.navCtrl.pop();
+
+    }
 } 
